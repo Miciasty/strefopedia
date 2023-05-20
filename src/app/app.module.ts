296ead5ea04167ea55e04c7b065e9e587f38bc56
Aleksander Antończak <alek4652@gmail.com>
2023-05-20 17:48:35 +0200

    Usunięto zbędne importy

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,6 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
-import { NavComponent } from './wiki/nav/nav.component';
-import { ItemComponent } from './wiki/nav/item/item.component';
-import { CategoryComponent } from './wiki/nav/category/category.component';
-import { MainComponent } from './wiki/main/main.component';
 import { HttpClientModule, HttpClient } from '@angular/common/http';
 import { MarkdownModule } from 'ngx-markdown'; 
 import { RouterModule } from '@angular/router';
